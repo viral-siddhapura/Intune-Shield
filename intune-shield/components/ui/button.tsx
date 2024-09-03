23da967adf0a6bd56f2b8cfbdc83c5bd158cc38d
HVMS <viralsid2330@gmail.com>
2024-09-03 18:15:13 -0300

    joinUs custom design button created

--- intune-shield/components/ui/button.tsx
+++ intune-shield/components/ui/button.tsx
@@ -9,6 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
+        joinUs: "w-[150px] bg-violet-700 text-white hover:bg-violet-700/90 rounded-full",
         default: "bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
         destructive:
           "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
