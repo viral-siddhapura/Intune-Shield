8f0ae4e17a9775de94747dd4e8e45bc995a96859
HVMS <viralsid2330@gmail.com>
2024-09-03 12:32:17 -0300

    about softec component completed

--- intune-shield/components/navigation/about-softec-title.tsx
+++ intune-shield/components/navigation/about-softec-title.tsx
@@ -1,7 +1,9 @@
 export const AboutSoftecTitle = () => {
     return (
-        <div className="static w-full h-96 bg-violet-800">
-            <div className="absolute flex rounded-lg top-72 left-1/4 bg-gray-200 w-1/2 h-96"></div>
+        <div className="static w-full">
+            <p className="absolute text-white text-7xl font-medium top-32 left-1/4 mx-4">About</p>
+            <p className="absolute text-violet-800 font-medium text-7xl top-52 left-1/4 z-10 mx-4">Softec</p>
+            <div className="absolute flex rounded-lg bg-gray-200 top-52 left-1/4 w-1/2 h-80"></div>
         </div>
     )
 }
