b255ba80d6df16baec80837cbe6294bbfe084ad0
HVMS <viralsid2330@gmail.com>
2024-09-03 12:31:51 -0300

    section: AboutSoftec, BusinessPartners Grid and Aboutcompany initiated

--- intune-shield/app/page.tsx
+++ intune-shield/app/page.tsx
@@ -1,14 +1,22 @@
+import { AboutCompany } from "@/components/aboutCompany/about-company";
+import { BusinessPartnersGrid } from "@/components/businessPartner/business-partner-grid";
 import { AboutSoftecTitle } from "@/components/navigation/about-softec-title";
-import { HeaderBottom } from "@/components/navigation/header-bottom";
 import { HeaderComponent } from "@/components/navigation/header-component";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen flex">
-      <div className="w-full h-96">
+    <div className="w-full flex flex-col">
+      <div className="w-full h-96 bg-violet-800 flex flex-col">
         <HeaderComponent />
         <AboutSoftecTitle />
-        {/* <HeaderBottom /> */}
+      </div>
+      <div className="w-full flex mt-72 flex-row items-center justify-center">
+        <div className="flex flex-row items-center justify-center">
+          <BusinessPartnersGrid />
+        </div>
+      </div>
+      <div className="w-full flex mt-12 flex-row items-center justify-center">
+        <AboutCompany />
       </div>
     </div>
   );
