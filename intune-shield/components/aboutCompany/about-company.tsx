0e831e128f79316e5d162a29f77f89753ce3486d
HVMS <viralsid2330@gmail.com>
2024-09-03 12:33:07 -0300

    About company component

---
+++ intune-shield/components/aboutCompany/about-company.tsx
@@ -0,0 +1,14 @@
+export const AboutCompany = () => {
+    return (
+        <div className="flex flex-row items-center justify-center">
+            <div className="grid grid-cols-2 gap-4">
+                <div>
+                    <p>Hello world</p>
+                </div>
+                <div>
+                    <p>Hello world</p>
+                </div>
+            </div>
+        </div>
+    )
+}
