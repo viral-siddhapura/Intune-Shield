a72b8a2a2ca4c566b33c2698d35753d68650f1fe
HVMS <viralsid2330@gmail.com>
2024-09-04 11:14:27 -0300

    init: footer component

--- intune-shield/components/footer/footer-component.tsx
+++ intune-shield/components/footer/footer-component.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 export const FooterSection = () => {
     return (
-        <footer>
-            <div className="grid grid-cols-4 grid-rows-4 gap-4">
+        <footer className="w-full flex flex-col items-center mt-72">
+            <Image className="absolute top-[4900px]" src="/icons/try-service-now-bg.svg" alt="logo" width={1170} height={340} />
+            <div className="grid grid-cols-4 grid-rows-4 gap-10">
                 <div className="row-span-4">
                     <div className="flex flex-row items-start">
                         <Image src="/logo.svg" alt="logo" width={34} height={36} />
@@ -45,12 +48,21 @@ export const FooterSection = () => {
                         <p className="text-black text-lg mt-4">Login</p>
                         <p className="text-black text-lg mt-4">Register</p>
                         <p className="text-black text-lg mt-4">404 Not found</p>
-                    </div></div>
+                    </div>
+                </div>
                 <div className="row-span-4">
                     <div className="flex flex-row items-start">
                         <p className="text-black text-2xl font-bold">Our Newsletter</p>
                     </div>
+                    <div className="flex w-full max-w-sm items-center space-x-2 mt-10">
+                        <Input type="email" placeholder="Email" />
+                        <Button variant={"sendEmail"} type="submit">Subscribe</Button>
+                    </div>
+                    <h3 className="text-lg font-semibold mt-8 mb-4">Social media</h3>
                 </div>
+            </div>
+            <div className="flex items-center justify-center mt-10">
+                Full Copyright & Design By @Softec – 2024
             </div>
         </footer>
     )
